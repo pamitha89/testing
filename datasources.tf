6e227ceae55a3e3b946a89fc5446ef312ac9cb59
pamitha89 <pamitha@live.com>
2024-06-19 12:09:08 +1000

    Create datasources.tf

---
+++ datasources.tf
@@ -0,0 +1,70 @@
+data "azurerm_resource_group" "arg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_private_dns_zone" "dnszone" {
+  provider = azurerm.CoreServices
+
+  for_each            = var.azurePrivateDNS
+  name                = each.value
+  resource_group_name = "arg-cre-r1-privatedns-01"
+}
+
+data "azurerm_client_config" "current" {}
+data "azurerm_subscription" "current" {
+}
+data "azurerm_private_dns_zone" "syn_st_zone_blob" {
+  provider = azurerm.CoreServices
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = "arg-cre-r1-privatedns-01"
+}
+
+data "azurerm_private_dns_zone" "syn_st_zone_dfs" {
+  provider = azurerm.CoreServices
+  name                = "privatelink.dfs.core.windows.net"
+  resource_group_name = "arg-cre-r1-privatedns-01"
+}
+
+# Linking of DNS zones to Virtual Network
+
+
+
+# Private Endpoint configuration
+
+resource "azurerm_private_endpoint" "syn_st_pe_blob" {
+  name                = "pe-${azurerm_storage_account.str.name}-blob"
+  location            = local.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = data.azurerm_subnet.pepstasbnt.id
+
+  private_service_connection {
+    name                           = "psc-blob-${local.basename}"
+    private_connection_resource_id = azurerm_storage_account.str.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group-blob"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.syn_st_zone_blob.id]
+  }
+}
+
+resource "azurerm_private_endpoint" "syn_st_pe_dfs" {
+  name                = "pe-${azurerm_storage_account.str.name}-dfs"
+  location            = local.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = data.azurerm_subnet.pepstasbnt.id
+
+  private_service_connection {
+    name                           = "psc-dfs-${local.basename}"
+    private_connection_resource_id = azurerm_storage_account.str.id
+    subresource_names              = ["dfs"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group-dfs"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.syn_st_zone_dfs.id]
+  }
+}
