6bf88930d19fdf8948137198ec9cdc33d8a50dd0
pamitha89 <pamitha@live.com>
2024-06-19 12:31:35 +1000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,7 +3,11 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "3.105.0"
-      configuration_aliases = [ azurerm.CoreServices]
+      #configuration_aliases = [ azurerm.CoreServices]
     }
   }
 }
+
+provider "azurerm" {
+  features {}
+}  
